c3d0837bb6ae3fca07557a5748f760b75179b750
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-17 10:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function appendKitten(name, array) {
 
 function prependKitten(name, array) {
   console.log (kittens.concat("Arnold"))
-  return kittens
+  
 }
 
 function removeLastKitten(name, array) {
